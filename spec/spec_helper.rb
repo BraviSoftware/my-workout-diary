d0f70eca3f88c20e0c046c6c2a89f78c1adf647d
Max Claus Nunes <maxcnunes@gmail.com>
2013-08-07 23:45:46 -0300

    configuring factory_girl

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,16 +10,8 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 RSpec.configure do |config|
 
-  config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.clean_with(:truncation)
-  end
-
   config.before(:each) do
-    DatabaseCleaner.start
-  end
-
-  config.after(:each) do
+    DatabaseCleaner.strategy = :truncation
     DatabaseCleaner.clean
   end
 
