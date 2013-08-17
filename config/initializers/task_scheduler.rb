85ca2d1337f9c02b4bb5ea3ae35b9fc87e60b671
Max Claus Nunes <maxcnunes@gmail.com>
2013-08-17 19:27:27 -0300

    log when run schedule to wake up Heroku

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -5,6 +5,7 @@ if Rails.env.production?
   scheduler.every '10m' do
      require "net/http"
      require "uri"
+     puts "### Waking up Heroku scheduler at #{Time.now}"
      Net::HTTP.get_response(URI.parse(ENV["HOSTNAME"]))
   end
 end
