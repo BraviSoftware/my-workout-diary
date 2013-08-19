be142a4b27d2328afe7c839f1393d3aa9e389f25
Max Claus Nunes <maxcnunes@gmail.com>
2013-08-19 07:16:58 -0300

    fixing user mailer tests :kissing_smiling_eyes:
    fixing mailer tests and email notification bar when user is not logged in :disappointed:
    
    fixing environment variables configuration for tests in travis ci

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,12 @@ module ApplicationHelper
   def user_activated_email_notification
     if current_user
       if current_user.receive_email_notification
-        badge('Activated - Email Notification', :success)
+        content = badge('Activated - Email Notification', :success)
       else
-        badge('Deactivated - Email Notification', :important)
+        content = badge('Deactivated - Email Notification', :important)
       end
+
+      link_to content, users_switch_email_notification_path, remote: true, method: :post
     end
   end
 end
