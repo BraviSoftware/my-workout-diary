6d68c29640d28721f62e1a1671e4cffba725da58
Max Claus Nunes <maxcnunes@gmail.com>
2013-09-10 01:26:16 -0300

    #8 - fixing some wrong english text

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,6 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 # Default Activity Types
-ActivityType.create(name: "Phisical Exercises", description: "Have you done phisical exercies?", status: true)
-ActivityType.create(name: "Drink Water", description: "Have you drank water?", status: true)
+ActivityType.create(name: "Physical Exercises", description: "Have you done physical exercies?", status: true)
+ActivityType.create(name: "Drink Water", description: "Have you drunk water?", status: true)
 ActivityType.create(name: "Fruits", description: "Have you eaten fruits?", status: true)
